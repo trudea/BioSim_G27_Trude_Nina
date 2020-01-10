c757a478d94d94a543f113c256c0adb896ebe047
NIna <nive@nmbu.no>
2020-01-10 11:02:14 +0100

    Created separate test file

--- src/biosim/test_nina.py
+++
@@ -1,8 +0,0 @@
-import src.BioSim_G27_Trude_Nina.island as bs
-import pytest
-
-class TestIsland:
-
-    def test_constructor_default_(self):
-        i = bs.Island()
-        assert isinstance(i, bs.Island)
