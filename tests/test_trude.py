9f1a43c22ba9c0c14c4354f65fc1968896c383b6
Trude Almestrand <trude_meg@hotmail.com>
2020-01-10 13:02:11 +0100

    Added testing for landscape and animals

--- tests/test_trude.py
+++ tests/test_trude.py
@@ -24,7 +24,62 @@ class TestIsland:
     def test_map_ocean(self):
         with pytest.raises(ValueError):
             bs.Island('SSS\nOOO')
+            bs.Island('OOO\nOSS')
+            bs.Island('OOO\nOSO\nOSO')
 
-    def test_savannah_instance(self):
-        savannah = bs.Island(rossum_string)
-        assert isinstance(savannah, bs.Island)
+    def test_jungle_instance(self):
+        jungle = bs.Jungle(None, None)
+        assert isinstance(jungle, bs.Jungle)
+
+    def test_desert_instance(self):
+        desert = bs.Desert(None, None)
+        assert isinstance(desert, bs.Desert)
+
+    def test_ocean(self):
+        ocean = bs.Ocean(None, None)
+        assert isinstance(ocean, bs.Ocean)
+
+    def test_mountain(self):
+        mountain = bs.Mountain(None, None)
+        assert isinstance(mountain, bs.Mountain)
+
+    def test_fodder_savannah(self):
+        savannah = bs.Savannah(None, None)
+        savannah.f = 200
+        savannah.replenish()
+        assert savannah.f > 200
+
+    def test_fodder_jungle(self):
+        jungle = bs.Jungle(None, None)
+        jungle.f = 500
+        jungle.replenish()
+        assert jungle.f == jungle.param_dict['f_max']
+
+
+class TestAnimal:
+
+    def test_herbivore(self):
+        herbivore = bs.Herbivore(None, None, None)
+        assert isinstance(herbivore, bs.Herbivore)
+
+    def test_carnivore(self):
+        carnivore = bs.Carnivore(None, None, None)
+        assert isinstance(carnivore, bs.Carnivore)
+
+    def test_aging(self):
+        herbivore = bs.Herbivore(None, None, None)
+        age = herbivore.age
+        herbivore.aging()
+        assert herbivore.age > age
+
+    def test_weightloss(self):
+        herbivore = bs.Herbivore(None, None, None)
+        weight = herbivore.weight
+        herbivore.weightloss()
+        assert herbivore.weight < weight
+
+    def test_herbivore_feeding(self):
+        herbivore = bs.Herbivore(None, None, None)
+        weight = herbivore.weight
+        #vent
+        herbivore.feeding(self, F)
