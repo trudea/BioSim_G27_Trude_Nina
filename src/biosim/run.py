1bc83fee2fbf6bc2211c7a7cba8f3bb5514d9302
Trude Almestrand <trude_meg@hotmail.com>
2020-01-12 10:45:19 +0100

    Fixed import issue

--- src/biosim/run.py
+++ src/biosim/run.py
@@ -7,9 +7,9 @@ __author__ = "Trude Haug Almestrand", "Nina Mariann Vesseltun"
 __email__ = "trude.haug.almestrand@nmbu.no", "nive@nmbu.no"
 
 
-from landscapes import Savannah, Jungle, Ocean, Mountain, Desert
-from animals import Herbivore, Carnivore, bubble_sort_animals
-from island import Island, Cell
+from .landscapes import Savannah, Jungle, Ocean, Mountain, Desert
+from .animals import Herbivore, Carnivore, bubble_sort_animals
+from .island import Island, Cell
 
 
 class Run:
