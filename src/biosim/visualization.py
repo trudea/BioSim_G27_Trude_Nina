134702e3f126c9320c31468dd00d3e15477f31d5
Trude Almestrand <trude_meg@hotmail.com>
2020-01-18 21:11:46 +0100

    Working on scatterplot for map

--- src/biosim/visualization.py
+++ src/biosim/visualization.py
@@ -15,6 +15,7 @@ import biosim.run as r
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+from biosim.simulation import BioSim
 from biosim.animals import Carnivore, Herbivore
 progname = os.path.basename(sys.argv[0])
 progversion = "0.1"
@@ -57,37 +58,6 @@ herbivore_input = [{'loc': (3, 4), 'pop': [
                         {'species': 'Herbivore', 'age': 3, 'weight': 7.3},
                         {'species': 'Herbivore', 'age': 5, 'weight': 8.1}]}]
 
-
-kart = open('rossum.txt').read()
-
-#                   R    G    B
-rgb_value = {'O': (0.0, 0.0, 1.0),  # blue
-             'M': (0.5, 0.5, 0.5),  # grey
-             'J': (0.0, 0.6, 0.0),  # dark green
-             'S': (0.5, 1.0, 0.5),  # light green
-             'D': (1.0, 1.0, 0.5)}  # light yellow
-
-kart_rgb = [[rgb_value[column] for column in row]
-            for row in kart.splitlines()]
-
-fig = plt.figure()
-
-axim = fig.add_axes([0.1, 0.1, 0.7, 0.8])  # llx, lly, w, h
-axim.imshow(kart_rgb)
-axim.set_xticks(range(len(kart_rgb[0])))
-axim.set_xticklabels(range(1, 1 + len(kart_rgb[0])))
-axim.set_yticks(range(len(kart_rgb)))
-axim.set_yticklabels(range(1, 1 + len(kart_rgb)))
-plt.grid(color='black', linestyle='-', linewidth=0.5)
-
-axlg = fig.add_axes([0.85, 0.1, 0.1, 0.8])  # llx, lly, w, h
-axlg.axis('off')
-for ix, name in enumerate(('Ocean', 'Mountain', 'Jungle',
-                           'Savannah', 'Desert')):
-    axlg.add_patch(plt.Rectangle((0., ix * 0.2), 0.3, 0.1,
-                                 edgecolor='none',
-                                 facecolor=rgb_value[name[0]]))
-    axlg.text(0.35, ix * 0.2, name, transform=axlg.transAxes)
 
 
 """
@@ -137,20 +107,52 @@ def plot(years):
 """
 
 
-def plotting_simulation(years):
+def biosimmap(kart):
+    #                   R    G    B
+    rgb_value = {'O': (0.0, 0.0, 1.0),  # blue
+                 'M': (0.5, 0.5, 0.5),  # grey
+                 'J': (0.0, 0.6, 0.0),  # dark green
+                 'S': (0.5, 1.0, 0.5),  # light green
+                 'D': (1.0, 1.0, 0.5)}  # light yellow
+
+    kart_rgb = [[rgb_value[column] for column in row]
+                for row in kart.splitlines()]
+
+    fig = plt.figure()
+
+    axim = fig.add_axes([0.1, 0.1, 0.7, 0.8])  # llx, lly, w, h
+    axim.imshow(kart_rgb)
+    axim.set_xticks(range(len(kart_rgb[0])))
+    axim.set_xticklabels(range(1, len(kart_rgb[0])))
+    axim.set_yticks(range(len(kart_rgb)))
+    axim.set_yticklabels(range(1, len(kart_rgb)))
+    plt.grid(color='black', linestyle='-', linewidth=0.5)
+
+    axlg = fig.add_axes([0.85, 0.1, 0.1, 0.8])  # llx, lly, w, h
+    axlg.axis('off')
+    for ix, name in enumerate(('Ocean', 'Mountain', 'Jungle',
+                               'Savannah', 'Desert')):
+        axlg.add_patch(plt.Rectangle((0., ix * 0.2), 0.3, 0.1,
+                                     edgecolor='none',
+                                     facecolor=rgb_value[name[0]]))
+        axlg.text(0.35, ix * 0.2, name, transform=axlg.transAxes)
+    plt.show()
+
+
+def plotting_simulation(years, map, seed):
     fig = plt.figure()
     ax = fig.add_subplot(211)
     px = fig.add_subplot(212)
     fig.show()
-    simulation = r.Run(default_input)
+    simulation = BioSim(herbivore_input, map, seed)
 
     x, y = [], []
     y_herb, y_carn = [], []
     for i in range(50):
-        simulation.island.map[(2, 2)].pop['Herbivore'].append(Herbivore())
+        simulation.map[(2, 2)].pop['Herbivore'].append(Herbivore())
     while True:
-        simulation.one_cycle()
-        x.append(simulation.years)
+        simulation.one_year()
+        x.append(simulation.year)
         y.append(simulation.num_animals_results[-1])
         y_herb.append(simulation.per_species_results[-1]['Herbivore'])
         y_carn.append(simulation.per_species_results[-1]['Carnivore'])
@@ -159,12 +161,29 @@ def plotting_simulation(years):
         px.plot(x, y_carn, color='red')
         fig.canvas.draw()
         plt.pause(0.1)
-        if simulation.years == 50:
+        if simulation.year == 50:
             for i in range(5):
-                simulation.island.map[(2, 2)].pop['Carnivore'].append(
+                simulation.map[(2, 2)].pop['Carnivore'].append(
                     Carnivore())
 
 
+def scatterplot_map(years):
+    # Hent animal abundance pd
+
+    girls_grades = [89, 90, 70, 89, 100, 80, 90, 100, 80, 34]
+    boys_grades = [30, 29, 49, 48, 100, 48, 38, 45, 20, 30]
+    grades_range = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    fig = plt.figure()
+    ax = fig.add_axes()
+    ax.scatter(grades_range, girls_grades, color='r')
+    ax.scatter(grades_range, boys_grades, color='b')
+    ax.set_xlabel('Grades Range')
+    ax.set_ylabel('Grades Scored')
+    ax.set_title('scatter plot')
+    plt.show()
+
 
 if __name__ == '__main__':
-    plotting_simulation(10).show()
+    rossum = open('rossum.txt').read()
+    biosimmap(rossum)
+    plotting_simulation(10, rossum, None)
