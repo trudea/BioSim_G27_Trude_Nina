607434c211f0ac51116974914920836172c3dd4d
Trude Almestrand <trude_meg@hotmail.com>
2020-01-20 16:37:47 +0100

    working on end visualization

--- src/biosim/simulation_new_testing.py
+++ src/biosim/simulation_new_testing.py
@@ -1,192 +1,283 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from biosim.simulation import BioSim
+
+default_seed = 33
+default_txt = open('rossum.txt').read()
+default_pop = [{'loc': (3, 4), 'pop': [
+        {'species': 'Herbivore', 'age': 10, 'weight': 12.5},
+        {'species': 'Herbivore', 'age': 9, 'weight': 10.3},
+        {'species': 'Carnivore', 'age': 14, 'weight': 10.3},
+        {'species': 'Carnivore', 'age': 5, 'weight': 10.1}]},
+                     {'loc': (4, 4),
+                      'pop': [
+                          {'species': 'Herbivore', 'age': 10, 'weight': 12.5},
+                          {'species': 'Carnivore', 'age': 3, 'weight': 7.3},
+                          {'species': 'Carnivore', 'age': 5, 'weight': 8.1}]}]
+
+
+simulation = BioSim(default_txt, default_pop, default_seed)
+herbivores = []
+carnivores = []
+years = 0
+years_list = []
+fig = plt.figure()
+plt.ion()
+ax = fig.add_subplot(111)
+x = np.linspace(0, 20, 1)
+herbivore_line, = ax.plot(x, 0, 'g-')
+carnivore_line, = ax.plot(x, 0, 'r-')
+for i in range(20):
+    simulation.one_year()
+    years += 1
+    herbivore = herbivores.append(sum(simulation.animal_distribution[
+                                       'Herbivore']))
+    carnivore = carnivores.append(sum(simulation.animal_distribution[
+                                       'Herbivore']))
+    years_list.append(years)
+    herbivore_line.set_ydata(herbivore)
+    carnivore_line.set_ydata(carnivore)
+    fig.canvas.draw()
+    plt.pause(1)
+
+
+def heatmap():
+    x = sim.animal_distribution
+    y = x.pivot('Row', 'Col', 'Herbivore')
+    ax = sns.heatmap(y, vmax=200)
+    plt.show()
+
+
 # -*- coding: utf-8 -*-
 
-"""
-"""
-import pandas as pd
-from src.biosim.island import Island as isl
-from src.biosim.animals import Animal, Herbivore, Carnivore
-from src.biosim.run import Run
-from src.biosim.landscapes import Savannah, Jungle, Mountain, Desert, Ocean
-from biosim.simulation import BioSim
+'''
+:mod:`randvis.simulation` provides the user interface to the package.
+Each simulation is represented by a :class:`DVSim` instance. On each
+instance, the :meth:`DVSim.simulate` method can be called as often as
+you like to simulate a given number of steps.
+The state of the system is visualized as the simulation runs, at intervals
+that can be chosen. The graphics can also be saved to file at regular
+intervals. By calling :meth:`DVSim.make_movie` after a simulation is complete,
+individual graphics files can be combined into an animation.
+.. note::
+   * This module requires the program ``ffmpeg`` or ``convert``
+     available from `<http://ffmpeg.org>` and `<http://imagemagick.org>`.
+   * You need to set the  :const:`_FFMPEG_BINARY` and :const:`_CONVERT_BINARY`
+     constants below to the command required to invoke the programs
+   * You need to set the :const:`_DEFAULT_FILEBASE` constant below to the
+     directory and file-name start you want to use for the graphics output
+     files.
+Example
+--------
+::
+    sim = DVSim((10, 15), 0.1, 12345, _DEFAULT_FILEBASE)
+    sim.simulate(50, 1, 5)
+    sim.make_movie()
+This code
+#. creates a system with a 10x15 matrix, sets the noise level to 0.1,
+   the random number generator seed to 12345 and specifies the filename
+   for output;
+#. performs a simulation of 50 steps, updating the graphics after each
+   step and saving a figure after each 5th step;
+#. creates a movie from the individual figures saved.
+'''
+
+__author__ = "Hans E Plesser, NMBU"
+
 import matplotlib.pyplot as plt
-import random
-__author__ = ""
-__email__ = ""
+import numpy as np
+import subprocess
+import os
+from randvis.diffsys import DiffSys
+
+# update these variables to point to your ffmpeg and convert binaries
+_FFMPEG_BINARY = 'ffmpeg'
+_CONVERT_BINARY = 'magick'
+
+# update this to the directory and file-name beginning
+# for the graphics files
+_DEFAULT_GRAPHICS_DIR = os.path.join('..', 'data')
+_DEFAULT_GRAPHICS_NAME = 'dv'
+_DEFAULT_MOVIE_FORMAT = 'mp4'  # alternatives: mp4, gif
 
 
-class BioSim:
-    def __init__(
-        self,
-        island_map,
-        ini_pop,
-        seed,
-        ymax_animals=None,
-        cmax_animals=None,
-        img_base=None,
-        img_fmt="png",
-    ):
+class DVSim(object):
+    """Provides user interface for simulation, including visualization."""
+
+    def __init__(self, sys_size, noise, seed,
+                 img_dir=None, img_name=_DEFAULT_GRAPHICS_NAME,
+                 img_fmt='png'):
         """
-        :param island_map: Multi-line string specifying island geography
-        :param ini_pop: List of dictionaries specifying initial population
-        :param seed: Integer used as random number seed
-        :param ymax_animals: Number specifying y-axis limit for graph showing animal numbers
-        :param cmax_animals: Dict specifying color-code limits for animal densities
-        :param img_base: String with beginning of file name for figures, including path
-        :param img_fmt: String with file type for figures, e.g. 'png'
-        If ymax_animals is None, the y-axis limit should be adjusted automatically.
-        If cmax_animals is None, sensible, fixed default values should be used.
-        cmax_animals is a dict mapping species names to numbers, e.g.,
-           {'Herbivore': 50, 'Carnivore': 20}
-        If img_base is None, no figures are written to file.
-        Filenames are formed as
-            '{}_{:05d}.{}'.format(img_base, img_no, img_fmt)
-        where img_no are consecutive image numbers starting from 0.
-        img_base should contain a path and beginning of a file name.
+        :param sys_size:  system size, e.g. (5, 10)
+        :type sys_size: (int, int)
+        :param noise: noise level
+        :type noise: float
+        :param seed: random generator seed
+        :type seed: int
+        :param img_dir: directory for image files; no images if None
+        :type img_dir: str
+        :param img_name: beginning of name for image files
+        :type img_name: str
+        :param img_fmt: image file format suffix, default 'png'
+        :type img_fmt: str
         """
-        self.island_map = island_map
-        self.ini_pop = ini_pop
-        if seed is not None:
-            self.seed = random.seed(seed)
-        self.ymax_animals = ymax_animals
-        self.cmax_animals = cmax_animals
-        self.img_base = img_base
-        self.img_fmt = img_fmt
-        self.year = 0
-        self.fig = None
-        self.ax1 = None
-        self.ax2 = None
-        self.ax3 = None
-        self.ax4 = None
-        self.x_lim = (0, 500)
-        self.y_lim = (0, 10000)
-        self.last_simulation = None
 
-    def biosimmap(self):
+        np.random.seed(seed)
+        self._system = DiffSys(sys_size, noise)
+
+        if img_dir is not None:
+            self._img_base = os.path.join(img_dir, img_name)
+        else:
+            self._img_base = None
+        self._img_fmt = img_fmt
+
+        self._step = 0
+        self._final_step = None
+        self._img_ctr = 0
+
+        # the following will be initialized by _setup_graphics
+        self._fig = None
+        self._map_ax = None
+        self._img_axis = None
+        self._mean_ax = None
+        self._mean_line = None
+
+    def simulate(self, num_steps, vis_steps=1, img_steps=None):
         """
-        Method that creates an RGB map from string
-
-        :return: figure of map
-
-        source: yngvem / INF200-2019/lectures/J05/mapping.py on github
+        Run simulation while visualizing the result.
+        :param num_steps: number of simulation steps to execute
+        :param vis_steps: interval between visualization updates
+        :param img_steps: interval between visualizations saved to files
+                          (default: vis_steps)
+        .. note:: Image files will be numbered consecutively.
         """
-        #                   R    G    B
-        rgb_value = {'O': (0.0, 0.0, 1.0),  # blue
-                     'M': (0.5, 0.5, 0.5),  # grey
-                     'J': (0.0, 0.6, 0.0),  # dark green
-                     'S': (0.5, 1.0, 0.5),  # light green
-                     'D': (1.0, 1.0, 0.5)}  # light yellow
 
-        kart_rgb = [[rgb_value[column] for column in row]
-                    for row in self.island_map.splitlines()]
+        if img_steps is None:
+            img_steps = vis_steps
 
-        fig = plt.figure()
+        self._final_step = self._step + num_steps
+        self._setup_graphics()
 
-        self.ax1 = fig.add_axes([0.1, 0.1, 0.7, 0.8])  # llx, lly, w, h
-        self.ax1.imshow(kart_rgb)
-        self.ax1.set_xticks(range(len(kart_rgb[0])))
-        self.ax1.set_xticklabels(range(1, len(kart_rgb[0])))
-        self.ax1.set_yticks(range(len(kart_rgb)))
-        self.ax1.set_yticklabels(range(1, len(kart_rgb)))
-        self.ax1.set_title('Map of the island')
-        plt.grid(color='black', linestyle='-', linewidth=0.5)
+        while self._step < self._final_step:
 
-        axlg = fig.add_axes([0.85, 0.1, 0.1, 0.8])  # llx, lly, w, h
-        axlg.axis('off')
-        for ix, name in enumerate(('Ocean', 'Mountain', 'Jungle',
-                                   'Savannah', 'Desert')):
-            axlg.add_patch(plt.Rectangle((0., ix * 0.2), 0.3, 0.1,
-                                         edgecolor='none',
-                                         facecolor=rgb_value[name[0]]))
-            axlg.text(0.35, ix * 0.2, name, transform=axlg.transAxes)
-        plt.show()
+            if self._step % vis_steps == 0:
+                self._update_graphics()
+
+            if self._step % img_steps == 0:
+                self._save_graphics()
+
+            self._system.update()
+            self._step += 1
+
+    def make_movie(self, movie_fmt=_DEFAULT_MOVIE_FORMAT):
+        """
+        Creates MPEG4 movie from visualization images saved.
+        .. :note:
+            Requires ffmpeg
+        The movie is stored as img_base + movie_fmt
+        """
+
+        if self._img_base is None:
+            raise RuntimeError("No filename defined.")
+
+        if movie_fmt == 'mp4':
+            try:
+                # Parameters chosen according to http://trac.ffmpeg.org/wiki/Encode/H.264,
+                # section "Compatibility"
+                subprocess.check_call([_FFMPEG_BINARY,
+                                       '-i',
+                                       '{}_%05d.png'.format(self._img_base),
+                                       '-y',
+                                       '-profile:v', 'baseline',
+                                       '-level', '3.0',
+                                       '-pix_fmt', 'yuv420p',
+                                       '{}.{}'.format(self._img_base,
+                                                      movie_fmt)])
+            except subprocess.CalledProcessError as err:
+                raise RuntimeError('ERROR: ffmpeg failed with: {}'.format(err))
+        elif movie_fmt == 'gif':
+            try:
+                subprocess.check_call([_CONVERT_BINARY,
+                                       '-delay', '1',
+                                       '-loop', '0',
+                                       '{}_*.png'.format(self._img_base),
+                                       '{}.{}'.format(self._img_base,
+                                                      movie_fmt)])
+            except subprocess.CalledProcessError as err:
+                raise RuntimeError(
+                    'ERROR: convert failed with: {}'.format(err))
+        else:
+            raise ValueError('Unknown movie format: ' + movie_fmt)
+
+    def _setup_graphics(self):
+        """Creates subplots."""
+
+        # create new figure window
+        if self._fig is None:
+            self._fig = plt.figure()
+
+        # Add left subplot for images created with imshow().
+        # We cannot create the actual ImageAxis object before we know
+        # the size of the image, so we delay its creation.
+        if self._map_ax is None:
+            self._map_ax = self._fig.add_subplot(2, 2, 1)
+            self._img_axis = None
+
+        # Add right subplot for line graph of mean.
+        if self._mean_ax is None:
+            self._mean_ax = self._fig.add_subplot(2, 2, 2)
+            self._mean_ax.set_ylim(0, 0.02)
+
+        # needs updating on subsequent calls to simulate()
+        self._mean_ax.set_xlim(0, self._final_step + 1)
+
+        if self._mean_line is None:
+            mean_plot = self._mean_ax.plot(np.arange(0, self._final_step),
+                                           np.full(self._final_step, np.nan))
+            self._mean_line = mean_plot[0]
+        else:
+            xdata, ydata = self._mean_line.get_data()
+            xnew = np.arange(xdata[-1] + 1, self._final_step)
+            if len(xnew) > 0:
+                ynew = np.full(xnew.shape, np.nan)
+                self._mean_line.set_data(np.hstack((xdata, xnew)),
+                                         np.hstack((ydata, ynew)))
+
+    def _update_system_map(self, sys_map):
+        '''Update the 2D-view of the system.'''
+
+        if self._img_axis is not None:
+            self._img_axis.set_data(sys_map)
+        else:
+            self._img_axis = self._map_ax.imshow(sys_map,
+                                                 interpolation='nearest',
+                                                 vmin=0, vmax=1)
+            plt.colorbar(self._img_axis, ax=self._map_ax,
+                         orientation='horizontal')
+
+    def _update_mean_graph(self, mean):
+        ydata = self._mean_line.get_ydata()
+        ydata[self._step] = mean
+        self._mean_line.set_ydata(ydata)
+
+    def _update_graphics(self):
+        """Updates graphics with current data."""
+
+        self._update_system_map(self._system.get_status())
+        self._update_mean_graph(self._system.mean_value())
+        plt.pause(1e-6)
+
+    def _save_graphics(self):
+        """Saves graphics to file if file name given."""
+
+        if self._img_base is None:
+            return
+
+        plt.savefig('{base}_{num:05d}.{type}'.format(base=self._img_base,
+                                                     num=self._img_ctr,
+                                                     type=self._img_fmt))
+        self._img_ctr += 1
 
 
-    @property
-    def year(self):
-        """Last year simulated."""
-        return self.year
-
-    @property
-    def num_animals(self):
-        """Total number of animals on island."""
-        return self.num_animals
-
-    @property
-    def num_animals_per_species(self):
-        """Number of animals per species in island, as dictionary."""
-        return self.num_animals_per_species
-
-    def animal_distribution(self):
-        coordinates = [cell for cell in self.map]
-        dictionary = {}
-        for cell in sim.map.values():
-            herbivore = 0
-            carnivore = 0
-            for species in cell.pop:
-                if species == 'Herbivore':
-                    herbivore += len(cell.pop[species])
-                if species == 'Carnivore':
-                    carnivore += len(cell.pop[species])
-            dictionary[cell] = \
-                (carnivore + herbivore), herbivore, carnivore
-
-        population = [dictionary[cell][0] for cell in dictionary]
-        herbivores = [dictionary[cell][1] for cell in dictionary]
-        carnivores = [dictionary[cell][2] for cell in dictionary]
-        coordinates = [cell for cell in self.map]
-        data = {'Cell': coordinates, 'Population': population,
-                'Herbivores': herbivores, 'Carnivores': carnivores}
-        return pd.DataFrame(data)
-
-    def plot_update(self, years, c_herb, c_carn):
-        if years == 0:
-            self.fig = plt.figure()
-        plt.axis('on')
-        if self.x_lim is None:
-            self.x_lim = years
-
-        self.ax1.imshow()
-        self.ax2.axis([years, years + self.x_lim, years, self.y_lim])
-        plot_herbivore = ax2.plot(np.arange(years), pd.DataFrame[
-            'Herbivores'], 'g-', label= 'Herbivores')
-        plot_carnivore = ax2.plot(np.arange(years), pd.DataFrame['Carnivore'],
-                                  'r-', label='Carnivores')
-
-        self.ax3.plt.title("Distribution Herbivore")
-        self.ax3.set_xticks(range(0, len(self.island_map)), 2)
-        self.ax3.set_xtickslabels(range(1, 1 + len(self.island_map)), 2)
-        self.ax3.set_yticks(range(0, len(self.island_map)), 2)
-        self.ax3.set_yticklabels(range(1, 1 + len(self.island_map)), 2)
-        ax3_distr = plt.imshow([[0 for _ in range(21)] for _ in range(13)])
-        if self.years == 0:
-            plt.colorbar(ax3_distr, orientation='horizontal', ticks =[])
-
-        self.ax4.plt.title("Distribution Carnivore")
-        self.ax4.set_xticks(range(0, len(self.island_map)), 2)
-        self.ax4.set_xtickslabels(range(1, 1 + len(self.island_map)), 2)
-        self.ax4.set_yticks(range(0, len(self.island_map)), 2)
-        self.ax4.set_yticklabels(range(1, 1 + len(self.island_map)), 2)
-        ax4_distr = plt.imshow([[0 for _ in range(21)] for _ in range(13)])
-        if self.years == 0:
-            plt.colorbar(ax4_distr, orientation='horizontal', ticks=[])
-
-        for n in xrange(self.years, self.years + years):
-            heatmap #lag heatmap vha pandas = self.heatmap(
-            # self.island.one_year()
-            ax3.imshow(self.heat[0], interpolation='nearest', cmap=c_herb)
-            ax4.imshow(self.heat[1], interpolation='nearest',cmap=c_carn)
-            herbs = np.sum(pd.DataFrame['Herbivores'])
-            carns = np.sum(pd.DataFrame['Carnivores'])
 
 
-        pass
-
-    def make_movie(self):
-        """Create MPEG4 movie from visualization images saved."""
-        pass
-
-    @year.setter
-    def year(self, value):
-        self._year = value
-
-if __name__ == '__main__':
