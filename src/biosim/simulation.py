7652527379401a3ce858cb0709361b95c148de7b
NIna <nive@nmbu.no>
2020-01-20 19:12:17 +0100

    Revert "Revert "Auto stash before merge of "master" and "Trude/master"""
    This reverts commit 515e21cf4f0c5a593b01a7217929ca25ac526b45.

--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -17,7 +17,7 @@ import random
 from src.biosim.landscapes import Savannah, Jungle, Desert, Mountain, Ocean
 import pandas as pd
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import subprocess
@@ -168,66 +168,7 @@ class BioSim:
         """
         self.land_dict[landscape].set_params(params)
 
-    def make_rgb_map(self):
-        # Add tp left subplot for images created with imshow().
-        # We cannot create the actual ImageAxis object before we know
-        # the size of the image, so we delay its creation.
-        ax1 = self._fig.add_subplot(221)
-        plt.title('Rossum Island')
-        #                   R    G    B
-        rgb_value = {'O': (0.0, 0.0, 1.0),  # blue
-                     'M': (0.5, 0.5, 0.5),  # grey
-                     'J': (0.0, 0.6, 0.0),  # dark green
-                     'S': (0.5, 1.0, 0.5),  # light green
-                     'D': (1.0, 1.0, 0.5)}  # light yellow
-
-        kart_rgb = [[rgb_value[column] for column in row]
-                    for row in self.island_map.splitlines()]
-
-        self.ax1 = fig.add_axes([0.1, 0.1, 0.7, 0.8])  # llx, lly, w, h
-        self.ax1.imshow(kart_rgb, interpolation='nearest')
-        self.ax1.set_xticks(range(len(kart_rgb[0])))
-        self.ax1.set_xticklabels(range(1, 1 + len(kart_rgb[0])))
-        self.ax1.set_yticks(range(len(kart_rgb)))
-        self.ax1.set_yticklabels(range(1, 1 + len(kart_rgb)))
-
-        axlg = self._fig.add_axes([0.85, 0.1, 0.1, 0.8])  # llx, lly, w, h
-        axlg.axis('off')
-        for ix, name in enumerate(('Ocean', 'Mountain', 'Jungle',
-                                   'Savannah', 'Desert')):
-            axlg.add_patch(plt.Rectangle((0., ix * 0.2), 0.3, 0.1,
-                                         edgecolor='none',
-                                         facecolor=rgb_value[name[0]]))
-            axlg.text(0.35, ix * 0.2, name, transform=axlg.transAxes)
-
-    def population_line_plot(self):
-        self.ax2.set_xlim(0, self.n_steps)
-        self.ax2.set_ylim(self.y_lim[0], self.y_lim[1])
-        self.ax2.set_title('Population')
-
-        if self.line_herbivore is None:
-            self.line_herbivore = self.ax2.plot(
-                np.arange(0, self.n_steps + 1, vis_steps),
-                np.nan * np.ones(
-                    len(np.arange(0, self.n_steps + 1, vis_steps))),'g-')
-
-            self.line_carnivore = self.ax2.plot(
-                np.arange(0, self.n_steps + 1, vis_steps),
-                np.nan * np.ones(
-                    len(np.arange(0, self.n_steps + 1, vis_steps))), 'r-')
-            self.ax2.legend(['Herbivore', 'Carnivore'])
-
-        else:
-            x, y = self.line_herbivore.get_data()
-            new_x = np.arange(x[-1] + 1, self.n_steps + 1, vis_steps)
-            if len(new_x > 0):
-                new_y = np.nan * np.ones_like(new_x)
-                self.line_herbivore.set_data(new_x, new_y)
-
-
-
-
-    def simulate(self, num_steps, vis_steps=1, img_steps=None):
+    def simulate(self, num_years, vis_years = 1, img_years=None):
         """
         Run simulation while visualizing the result.
         :param num_steps: number of simulation steps to execute
@@ -246,27 +187,29 @@ class BioSim:
         while self._step < self._final_step:
 
             if self._step % vis_steps == 0:
-                self._update_graphics()
+                """ self._update_graphics()"""
+                pass
 
-            if self._step % img_steps == 0:
-                self._save_graphics()
+            #if self._step % img_steps == 0:
+            #    self._save_graphics()
 
-            self._system.update()
-            self._step += 1
+            #self._system.update()
+            #self._step += 1
 
         while (self.sim_years < self.num_years):
             self.one_year()
             self.sim_years += 1
             # print(self.year, ' ', self.num_animals_per_species)
-            for cell in self.map.values():
-                for key in self.change:
-                    for species in self.change[key]:
-                        self.change[key][species] += cell.change[key][species]
+
+
 
     def _setup_graphics(self):
         """Creates subplots."""
 
         # create new figure window
+
+        pass
+        """
         if self._fig is None:
             self._fig = plt.figure()
 
@@ -280,6 +223,7 @@ class BioSim:
 
         # needs updating on subsequent calls to simulate()
         self._mean_ax2.set_xlim(0, self._final_step + 1)
+        """
 
 
     def one_year(self):
@@ -299,6 +243,11 @@ class BioSim:
         self.year = 1
         # print(self.num_animals_per_species)
         # print(self.year, ' ', self.change)
+        for cell in self.map.values():
+            for key in self.change:
+                for species in self.change[key]:
+                    self.change[key][species] += cell.change[key][species]
+        print(self.change)
 
     def add_population(self, population):
         """
@@ -445,15 +394,15 @@ if __name__ == '__main__':
                OOOOOOOOOOOOOOOOOOOOO'
     geogr = textwrap.dedent(geogr)
     """
-    # default_txt = open('rossum.txt').read()
-    default_txt = 'OOOOO\nOJJJO\nOJJJO\nOJJJO\nOOOOO'
+    default_txt = open('rossum.txt').read()
+    # default_txt = 'OOOOO\nOJJJO\nOJJJO\nOJJJO\nOOOOO'
 
     ini_herbs = [
         {
             "loc": (2, 2),
             "pop": [
                 {"species": "Herbivore", "age": 5, "weight": 20}
-                for _ in range(3)
+                for _ in range(150)
             ],
         }
     ]
@@ -462,12 +411,12 @@ if __name__ == '__main__':
             "loc": (2, 2),
             "pop": [
                 {"species": "Carnivore", "age": 5, "weight": 20}
-                for _ in range(3)
+                for _ in range(150)
             ],
         }
     ]
 
-    sim = BioSim('OOOOO\nODJMO\nOJJSO\nOJSDO\nOOOOO', ini_herbs, 1)
-    sim.add_population(ini_carns)
-    sim.one_year()
+    sim = BioSim(default_txt, ini_herbs, 1)
+
+    sim.simulate(num_years=100, vis_years=1, img_years=2000)
 
