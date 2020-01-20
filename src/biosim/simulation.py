18967b85271b4449ca1b780f2db38ab280a38598
Trude Almestrand <trude_meg@hotmail.com>
2020-01-20 17:00:39 +0100

    Synchronizing

--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -266,15 +266,15 @@ class BioSim:
 
 
         # Add top right subplot for line graph of mean.
-        if self._mean_ax is None:
-            self._mean_ax = self._fig.add_subplot(2, 2, 2)
-            self._mean_ax.set_ylim(0, 0.02)
+        if self._mean_ax2 is None:
+            self._mean_ax2 = self._fig.add_subplot(2, 2, 2)
+            self._mean_ax2.set_ylim(0, 0.02)
 
         # needs updating on subsequent calls to simulate()
-        self._mean_ax.set_xlim(0, self._final_step + 1)
+        self._mean_ax2.set_xlim(0, self._final_step + 1)
 
         if self._mean_line is None:
-            mean_plot = self._mean_ax.plot(np.arange(0, self._final_step),
+            mean_plot = self._mean_ax2.plot(np.arange(0, self._final_step),
                                            np.full(self._final_step, np.nan))
             self._mean_line = mean_plot[0]
         else:
